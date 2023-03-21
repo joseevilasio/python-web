2bfe495b02c50cca9875d1fdb3a4b7c4860c9ac2
joseevilasio <jose_evilasio@outlook.com>
2023-03-21 11:46:07 +0000

    adicionado a feature de controller e commands CLI

--- day02/flask/blog/app.py
+++ day02/flask/blog/app.py
@@ -1,36 +1,8 @@
-from flask import Flask, url_for
-from flask_pymongo import PyMongo
+from flask import Flask
+from .config import configure
 
 
-app = Flask(__name__)
-
-
-app.config["APP_NAME"] = "Meu Blog"
-app.config["MONGO_URI"] = "mongodb://localhost:27017/blog"
-
-mongo = PyMongo(app)
-
-
-# Adicionamos um error handler
-@app.errorhandler(404)
-def not_found_page(error):
-    return f"<strong>Desculpe</strong> página não encontrada em {app.config['APP_NAME']}"
-                
-
-@app.route("/")
-def index():
-    posts = mongo.db.posts.find()  # NEW
-    content_url = url_for("read_content", slug="qualquer-coisa")
-    return (
-        f"<h1>Boas vindas a {app.config['APP_NAME']}</h1>"
-        f"<a href='{content_url}'>Leia um post</a>"
-        "<hr>"       
-        f"{list(posts)}"  # NEW
-    )
-
-
-def read_content(slug):
-    index_url = url_for("index")
-    return f"<h1>{slug}</h1><a href='{index_url}'>Voltar ao inicio</a>"
-
-app.add_url_rule("/<string:slug>", view_func=read_content)
+def create_app():
+    app = Flask(__name__)
+    configure(app)
+    return app
